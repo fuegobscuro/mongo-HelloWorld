bc5ac3f1b154dc41191488aaf008d0b870c4f879
Ignacio Fosco <nachieee@gmail.com>
2024-04-14 06:27:58 -0300

    Update index.js

--- client/server/index.js
+++ client/server/index.js
@@ -1,32 +1,9 @@
-require('dotenv').config();
-const connectDB = require('./src/database');
-const app = require('./src/server');
-// const seedDB = require('./src/data/seedDB');
+// // server/index.js
+const express = require('express');
+const app = express();
 
-app.use((req, res, next) => {
-  console.log('Received request:', req.method, req.path);
-  next();
+app.get('/api/test', (req, res) => {
+  res.send('Server is running!');
 });
-
-async function initializeApp() {
-  try {
-    await connectDB();
-    // await seedDB();
-
-    if (process.env.NODE_ENV === 'development') {
-      const PORT = process.env.PORT || 3001;
-      app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-      });
-    } else {
-      console.log('Server running in production mode');
-    }
-  } catch (error) {
-    console.error('Failed to connect to the database:', error);
-    process.exit(1);
-  }
-}
-
-initializeApp();
 
 module.exports = app;
