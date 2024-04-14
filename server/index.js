0ab66cb299f0b4cc28afe5ebbd123ca9f3195603
Ignacio Fosco <nachieee@gmail.com>
2024-04-14 01:41:36 -0300

    asddddd

--- server/index.js
+++ server/index.js
@@ -2,19 +2,18 @@ const connectDB = require('./src/database');
 const server = require('./src/server');
 // const seedDB = require('./src/data/seedDB');
 require('dotenv').config();
-const PORT = process.env.PORT || 3001;
-
-const startServer = () => {
-  server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-};
 
 async function initializeApp() {
   try {
     await connectDB();
     // await seedDB();
-    startServer();
+
+    if (process.env.NODE_ENV === 'development') {
+      const PORT = process.env.PORT || 3001;
+      app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+      });
+    }
   } catch (error) {
     console.error('Failed to connect to the database:', error);
     process.exit(1);
@@ -22,3 +21,5 @@ async function initializeApp() {
 }
 
 initializeApp();
+
+module.exports = app;
