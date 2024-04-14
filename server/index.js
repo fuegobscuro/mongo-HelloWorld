1d87b3185d39dd23c9888ce438d04570f83070fa
Ignacio Fosco <nachieee@gmail.com>
2024-04-13 22:50:53 -0300

    Que baile CORS / Vercel
    Bien jugado! :D

--- server/index.js
+++ server/index.js
@@ -1,24 +1,26 @@
 const connectDB = require('./src/database');
-const server = require('./src/server');
-// const seedDB = require('./src/data/seedDB');
+const app = require('./src/server');
 require('dotenv').config();
-const PORT = process.env.PORT || 3001;
-
-const startServer = () => {
-  server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-};
+// const seedDB = require('./src/data/seedDB');
 
 async function initializeApp() {
   try {
     await connectDB();
     // await seedDB();
-    startServer();
+    // startServer();
+    if (process.env.NODE_ENV === 'development') {
+      const PORT = process.env.PORT || 3001;
+      app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+      });
+      console.log('Connected to server successfully.');
+    }
   } catch (error) {
-    console.error('Failed to connect to the database:', error);
+    console.error('Failed to connect to the server:', error);
     process.exit(1);
   }
 }
 
 initializeApp();
+
+module.exports = app;
