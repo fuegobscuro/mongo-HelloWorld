e3d21151ad060febac3fe28e9dca7d31a1b74264
Ignacio Fosco <nachieee@gmail.com>
2024-04-14 05:20:41 -0300

    asd
    asd

--- client/server/index.js
+++ client/server/index.js
@@ -3,6 +3,11 @@ const connectDB = require('./src/database');
 const app = require('./src/server');
 // const seedDB = require('./src/data/seedDB');
 
+app.use((req, res, next) => {
+  console.log('Received request:', req.method, req.path);
+  next();
+});
+
 async function initializeApp() {
   try {
     await connectDB();
@@ -13,6 +18,8 @@ async function initializeApp() {
       app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
       });
+    } else {
+      console.log('Server running in production mode');
     }
   } catch (error) {
     console.error('Failed to connect to the database:', error);
